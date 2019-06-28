8355566989d4528cb903f81889f808587f10c0b5
徐畅 <xuc@changyi.com>
2019-06-28 15:07:40 +0800

    增加 链表结构

---
+++ code/LeetCode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using List;
+
+namespace LeetCode
+{
+    public class Program 
+    {
+        //001 两数相加
+        //static void Main(string[] args)
+        //{
+        //    int[] a = { 2, 3, 4, 5 };
+        //    var target = 5;
+        //    bool flag = true;
+        //    for (var i = 0; i < a.Length; i++)
+        //    {
+        //        for (var j = 0; j < a.Length; j++)
+        //        {
+        //            if (a[i] + a[j] == target)
+        //            {
+        //                flag = false;
+        //                int[] b = {i, j };
+        //                Console.WriteLine("{0},{1}", b[0], b[1]);
+        //                break;
+        //            }
+        //        }
+        //        if (!flag)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+        static void Main()
+        {
+            Lists a = new Lists();
+            for(int i = 1;i<=10;i++)
+            {
+                a.Insert(i);
+            }
+            Console.WriteLine(a.CurrentNode());
+            while(!a.IsEnd())
+            {
+                Console.WriteLine(a.NextNode());
+            }
+            a.Reset();
+            while(!a.IsEnd())
+            {
+                a.Remove();
+            }
+            a.Remove();
+            a.Reset();
+            if(a.IsEmpty())
+            {
+                Console.WriteLine("链表为空！");
+            }
+            Console.WriteLine("按任意键退出");
+            try
+            {
+                Console.Read();
+            }
+            catch(IOException e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ code/List/List.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace List
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public class Lists
+    {
+        private Node Head = null;
+        private Node Tail = null;
+        private Node Pointer = null;
+        private int Length = 0;
+
+        public void DeleteAll()
+        {
+            Head = null;
+            Tail = null;
+            Pointer = null;
+            Length = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return (Length == 0);
+        }
+
+        public void Reset()
+        {
+            Pointer = null;
+        }
+
+        public bool IsEnd()
+        {
+            if (Length == 0)
+            {
+                throw new Exception();
+            }
+            else if (Length == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return (Cursor() == Tail);
+            }
+        }
+
+        private Node Cursor()
+        {
+            if (Head == null)
+            {
+                throw new Exception();
+            }
+            else if (Pointer == null)
+            {
+                return Head;
+            }
+            else
+            {
+                return Pointer.next;
+            }
+        }
+
+        public object NextNode()
+        {
+            if (Length == 1)
+            {
+                throw new Exception();
+            }
+            else if (Length == 0)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                Node temp = Cursor();
+                Pointer = temp;
+                if (temp != Tail)
+                {
+                    return (temp.next.data);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        public object CurrentNode()
+        {
+            Node temp = Cursor();
+            return temp.data;
+        }
+
+        public void Insert(int d)
+        {
+            Node e = new Node(d);
+            if (Length == 0)
+            {
+                Tail = e;
+                Head = e;
+            }
+            else
+            {
+                Node temp = Cursor();
+                e.next = temp;
+                if (Pointer == null)
+                {
+                    Head = e;
+                }
+                else
+                {
+                    Pointer.next = e;
+                }
+            }
+            Length++;
+        }
+
+        public object Remove()
+        {
+            object temp;
+            if (Length == 0)
+            {
+                throw new Exception();
+            }
+            else if (Length == 1)
+            {
+                temp = Head.data;
+                DeleteAll();
+            }
+            else
+            {
+                Node cur = Cursor();
+                temp = cur.data;
+                if (cur == Head)
+                {
+                    Head = cur.next;
+                }
+                else if (cur == Tail)
+                {
+                    Pointer.next = null;
+                    Tail = Pointer;
+                    Reset();
+                }
+                else
+                {
+                    Pointer.next = cur.next;
+                }
+                Length--;
+            }
+            return temp;
+        }
+
+    }
+}
