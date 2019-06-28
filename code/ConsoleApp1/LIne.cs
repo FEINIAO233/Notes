14ef32f9063acaa1914767be2369611be6db09f6
徐畅 <xuc@changyi.com>
2019-06-28 11:40:06 +0800

    新建项目 LeetCode

---
+++ code/ConsoleApp1/LIne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+    }
+
+    public class Line
+    {
+        private Node Head = null;
+        private Node Tail = null;
+        private Node Pointer = null;
+        private int Length = 0;
+    }
+
+}
