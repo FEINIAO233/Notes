e01ca2ea9e048cb801dd4d260fd825f21ec9ebc8
feiniao <664836141@qq.com>
2020-03-10 18:01:27 +0800

    新增LeetCodeJava 题目

---
+++ LeetCodeJava/1.两数之和.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        var temp = new int[2];
+        for(var i = 0;i<nums.length;i++){
+            for(var j = 0;j<nums.length;j++){
+                if(i != j){
+                    if(nums[i] + nums[j] == target){
+                        temp[0] = i;
+                        temp[1] = j;
+                        return temp;
+                    }
+                }
+            }
+        }
+        return temp;
+    }
+}
+// @lc code=end
+
